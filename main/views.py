37e37b539bc46663af442bfb62cea820750beca9
kalnynsh <kda869@yandex.ru>
2017-04-23 19:28:09 +0300

    In /main/view create classes: MovieListView, MovieDetailsView, NewReviewView.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http.response import HttpResponseRedirect
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import ListView
 
-# Create your views here.
+from main.models import MovieDetails
+from main.models import MovieReview
+
+
+class MovieListView(ListView):
+    model = MovieDetails
+    template_name = "main/movies_list.html"
+
+
+class MovieDetailsView(DetailView):
+    model = MovieDetails
+    template_name = "main/movie_details.html"
+
+
+class NewReviewView(CreateView):
+    model = MovieReview
+    fields = ['user_name', 'review']
+
+    template_name = 'main/new_movie_review.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(NewReviewView, self).get_context_data(**kwargs)
+
+        movie_pk = self.kwargs['movie_pk']
+        movie = MovieDetails.objects.get(pk=movie_pk)
+        ctx['movie'] = movie
+
+        return ctx
+
+    def form_valid(self, form):
+        movie_pk = self.kwargs['movie_pk']
+        movie = MovieDetails.objects.get(pk=movie_pk)
+
+        review = form.save(commit=False)
+        review.movie = movie
+        review.save()
+
+        return HttpResponseRedirect(reverse('movie-details'), kwargs={'pk': movie_pk})
+
+
+
+
+
