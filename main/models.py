d1372b5d699b654672420290f331a3809886f4e4
kalnynsh <kda869@yandex.ru>
2017-04-23 18:35:58 +0300

    In /main/models create classes MovieDetails, MovieReview. In /settings add main to INSTALLED_APP.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class MovieDetails(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    stars = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class MovieReview(models.Model):
+    movie = models.ForeignKey(MovieDetails, related_name='reviews')
+    user_name = models.CharField(max_length=100)
+    review = models.TextField()
