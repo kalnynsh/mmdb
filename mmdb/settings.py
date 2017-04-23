c3604f87d3fc3babb9bee3f4a4e4c2c7e445fd13
kalnynsh <kda869@yandex.ru>
2017-04-23 17:28:29 +0300

    Create constants TEMPLATE_DIR path to 'templates', STATIC_DIR path to 'static', change TIME_ZONE='Europe/Moscow'.

--- mmdb/settings.py
+++ mmdb/settings.py
@@ -51,10 +51,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mmdb.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
